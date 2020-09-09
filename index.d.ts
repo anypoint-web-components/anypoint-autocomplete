4e9377671a541ca92abbf7074bce63173a924d5f
Pawel <jarrodek@gmail.com>
2020-09-08 18:46:53 -0700

    feat: adding types export

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,13 @@
-export { AnypointAutocomplete } from './src/AnypointAutocomplete';
+export {
+  AnypointAutocomplete,
+  Suggestion,
+  suggestionsValue,
+  processSource,
+  normalizeSource,
+  itemTemplate,
+  readLabelValue,
+  rippleTemplate,
+  openedValue,
+  openedValuePrivate,
+  autocompleteFocus,
+} from './src/AnypointAutocomplete';
